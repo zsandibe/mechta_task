35c855a9b72d1b0217650667840f95f8659540bf
zsandibe <Sandibekov1998@gmail.com>
2024-07-06 01:48:48 +0500

    upd: успешно зарефакторил.DONE

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -11,12 +13,31 @@ type Data struct {
 }
 
 func main() {
-	numGoroutines := parseFlags()
-	dataArray := readAndUnmarshalJson("data.json")
-	results := processItemsConcurrently(dataArray, numGoroutines)
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: go run . [json file path] [num of workers]")
+		return
+	}
+
+	filePath := os.Args[1]
+	numWorkers, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("Invalid number of workers:", err)
+		return
+	}
+
+	if numWorkers < 1 || numWorkers > 5000 {
+		fmt.Println("Number of workers must be between 1 and 5000")
+		return
+	}
+
+	pairs, err := readAndUnmarshal(filePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	startTime := time.Now()
-	totalSum := aggregateResults(results)
+	totalSum := parallelSum(pairs, numWorkers)
 	endTime := time.Now()
-	fmt.Printf("Total sum: %d\n", totalSum)
-	fmt.Printf("Time duration: %v\n", endTime.Sub(startTime))
+	fmt.Println("Total sum:", totalSum)
+	fmt.Println("Time duration:", endTime.Sub(startTime))
 }
--- utils.go
+++ utils.go
@@ -2,66 +2,60 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"runtime"
+	"path/filepath"
 	"sync"
 )
 
-func parseFlags() int {
-	var numGoroutines int
-	flag.IntVar(&numGoroutines, "goroutines", runtime.NumCPU(), "number of goroutines to use")
-	flag.Parse()
-	return numGoroutines
-}
+func readAndUnmarshal(filePath string) ([]Data, error) {
+	if filepath.Ext(filePath) != ".json" {
+		return nil, errors.New("File is not a JSON file")
+	}
 
-func readAndUnmarshalJson(fileName string) []Data {
-	content, err := ioutil.ReadFile(fileName)
+	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		fmt.Printf("Error reading file: %s\n", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("Error reading file: %w", err)
 	}
-	var dataArray []Data
-	if err = json.Unmarshal(content, &dataArray); err != nil {
-		fmt.Printf("Error unmarshaling json: %s\n", err)
-		os.Exit(1)
+
+	var pairs []Data
+	if err := json.Unmarshal(data, &pairs); err != nil {
+		return nil, fmt.Errorf("Error unmarshalling json: %w", err)
 	}
-	return dataArray
+
+	return pairs, nil
 }
 
-func processItemsConcurrently(dataArray []Data, numGoroutines int) chan int {
-	jobs := make(chan Data, len(dataArray))
-	results := make(chan int, len(dataArray))
-
+func parallelSum(pairs []Data, goroutineCount int) int {
+	results := make(chan int, goroutineCount)
 	var wg sync.WaitGroup
-	wg.Add(numGoroutines)
-	for i := 0; i < numGoroutines; i++ {
-		go worker(jobs, results, &wg)
-	}
 
-	for _, data := range dataArray {
-		jobs <- data
+	chunkSize := (len(pairs) + goroutineCount - 1) / goroutineCount
+	for i := 0; i < len(pairs); i += chunkSize {
+		end := i + chunkSize
+		if end > len(pairs) {
+			end = len(pairs)
+		}
+
+		wg.Add(1)
+		go func(pairsChunk []Data) {
+			defer wg.Done()
+			sum := 0
+			for _, pair := range pairsChunk {
+				sum += pair.A + pair.B
+			}
+			results <- sum
+		}(pairs[i:end])
 	}
-	close(jobs)
 
 	wg.Wait()
 	close(results)
-	return results
-}
 
-func worker(jobs <-chan Data, results chan<- int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for job := range jobs {
-		results <- job.A + job.B
-	}
-}
-
-func aggregateResults(results chan int) int {
 	totalSum := 0
-	for result := range results {
-		totalSum += result
+	for sum := range results {
+		totalSum += sum
 	}
+
 	return totalSum
 }
